40de6f3d032074c158a7e3730372a9dc3d630e0c
Solomon Nsumei <solnsumei@gmail.com>
2022-12-17 11:28:17 +0100

    use initializers in main

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/solnsumei/api-starter-template/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
 	fmt.Println("Welcome to API Starter template")
