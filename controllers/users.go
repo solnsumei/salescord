00f52aa72bdfac78d8cda1b8b7cd70b50b497683
Solomon Nsumei <solnsumei@gmail.com>
2023-01-18 09:51:43 +0100

    update user model to include name in testing

--- controllers/users.go
+++ controllers/users.go
@@ -21,6 +21,7 @@ func NewUsersController(db *gorm.DB) *UsersController {
 }
 
 type inputBody struct {
+	Name     string `json:"name" binding:"required,min=3,max=30"`
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=5"`
 }
